f687e096c2303e875f8e2fcbe1f6277fd91f1ed1
SILVAJESUS2005 <159391539+SILVAJESUS2005@users.noreply.github.com>
2024-09-25 22:52:58 -0600

    Se hicieron dos clases, se dio inicio al subproblema 4.

---
+++ src/unidad1/proglineal/GaussR.java
@@ -0,0 +1,30 @@
+/* ITESS-TICS-2024.
+* AGOSTO-DICIEMBRE 2024.
+* Matematicas para la toma de decisiones.
+* Descripción: Metodo GaussR.
+* TI302 JLS.
+* 23 DE SEPTIEMBRE DEL 2024.
+* By Jesús López Silva.
+* jesuslopezsilva206@gmail.com
+* Teacher Francisco Javier Montecillo Puente.
+ */
+package unidad1.proglineal;
+
+public class GaussR {
+
+    private double[][] matrizA;
+    private int R;
+    private int C;
+
+    public GaussR(double[][] matriz) {
+        this.matrizA = matriz;
+        this.R = matriz.length;
+        this.C = matriz[0].length;
+    }
+
+    }
+
+
+
+
+
---
+++ src/unidad1/proglineal/GaussRTest.java
@@ -0,0 +1,28 @@
+/* ITESS-TICS-2024.
+* AGOSTO-DICIEMBRE 2024.
+* Matematicas para la toma de decisiones.
+* Descripción: Metodo GaussRTest.
+* TI302 JLS.
+* 23 DE SEPTIEMBRE DEL 2024.
+* By Jesús López Silva.
+* jesuslopezsilva206@gmail.com
+* Teacher Francisco Javier Montecillo Puente.
+ */
+package unidad1.proglineal;
+
+public class GaussRTest {
+     public static void main(String[] args) {
+        // Ejemplo de entrada (primera matriz que diste)
+        double[][] matriz = {
+            {1.0000, -60.0000, -30.0000, -20.0000, 0, 0, 0, 0},
+            {0, 8.0000, 6.0000, 1.0000, 1, 0, 0, 48.0000},
+            {0, 4.0000, 2.0000, 1.5000, 0, 1, 0, 20.0000},
+            {0, 2.0000, 1.5000, 0.5000, 0, 0, 1, 8.0000},
+            {0, 0.0000, 1.0000, 0.0000, 0, 0, 0, 5.0000}
+        };
+
+
+
+    }
+}
+
