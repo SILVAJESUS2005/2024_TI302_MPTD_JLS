e94684016b086d1bd41db88814ed07d78a4cb630
SILVAJESUS2005 <159391539+SILVAJESUS2005@users.noreply.github.com>
2024-09-26 07:51:40 -0600

    Se implemento las dos clases GaussR  y GaussRTest, con sus casos de prueba y su OutPut correspondiemte.

--- src/unidad1/proglineal/GaussR.java
+++ src/unidad1/proglineal/GaussR.java
@@ -12,19 +12,48 @@ package unidad1.proglineal;
 
 public class GaussR {
 
-    private double[][] matrizA;
+    private double[][] A;
     private int R;
     private int C;
 
-    public GaussR(double[][] matriz) {
-        this.matrizA = matriz;
-        this.R = matriz.length;
-        this.C = matriz[0].length;
+    public GaussR(double[][] A, int R, int C) {
+        this.A = A;
+        this.R = R;
+        this.C = C;
     }
 
+    public void setA(double[][] A) {
+        this.A = A;
     }
 
+    public void setR(int R) {
+        this.R = R;
+    }
 
+    public void setC(int C) {
+        this.C = C;
+    }
 
+    public void Calular() {
+        double divisor = A[R][C];
+        for (int i = 0; i < A[R].length; i++) {
+            A[R][i] /= divisor;
+        }
 
+        for (int i = 0; i < A.length; i++) {
+            if (i != R) {
+                double factor = A[i][C];
+                for (int j = 0; j < A[i].length; j++) {
+                    A[i][j] -= factor * A[R][j];
+                }
+            }
+        }
 
+        for (double[] row : A) {
+            for (double value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
