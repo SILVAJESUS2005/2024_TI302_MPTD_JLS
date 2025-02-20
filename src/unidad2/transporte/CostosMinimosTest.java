b06e2409a4396ed477d74a4d24549ed9f7afc32e
SILVAJESUS2005 <159391539+SILVAJESUS2005@users.noreply.github.com>
2025-02-20 16:01:38 -0600

    Se creo el arbol.

--- src/unidad2/transporte/CostosMinimosTest.java
+++ src/unidad2/transporte/CostosMinimosTest.java
@@ -2,11 +2,11 @@ package unidad2.transporte;
 
 public class CostosMinimosTest {
     public static void main(String[] args) {
-        double O[] = {15, 15}; //valores de cada oferta
-        double D[] = {10, 10, 10}; //valores de cada demanda
-        double MatrizCostos[][] = {{4, 2, 4}, {12, 8, 4}}; // matriz de cada costo que hay (Aun no se ocupa)
-        
-        CostosMinimos case1 = new CostosMinimos(2, 3); // n es la cantidad de ofertas y m la cantidad de demandas
+        double O[] = {400, 200, 150}; //valores de cada oferta
+        double D[] = {100, 200, 150, 160, 140}; //valores de cada demanda
+        double MatrizCostos[][] = {{100, 150, 200, 140, 35}, {50, 70, 60, 65, 80}, {40, 90, 100, 150, 130}}; // matriz de cada costo que hay 
+
+        CostosMinimos case1 = new CostosMinimos(3, 5); // n es la cantidad de ofertas y m la cantidad de demandas
         case1.setD(D);
         case1.setO(O);
         case1.setMatrizCostos(MatrizCostos);
--- src/unidad2/transporte/EsquinaNoroesteTest.java
+++ src/unidad2/transporte/EsquinaNoroesteTest.java
@@ -4,11 +4,11 @@ package unidad2.transporte;
 
 public class EsquinaNoroesteTest {
     public static void main(String[] args) {
-        double O[] = {5, 10, 15}; //valores de cada oferta
-        double D[] = {3, 3, 12, 12}; //valores de cada demanda
-        double MatrizCostos[][] = {{20, 11, 3, 6}, {5, 9, 10, 2}, {18, 7, 4, 1}}; // matriz de cada costo que hay 
+        double O[] = {400, 200, 150}; //valores de cada oferta
+        double D[] = {100, 200, 150, 160, 140}; //valores de cada demanda
+        double MatrizCostos[][] = {{100, 150, 200, 140, 35}, {50, 70, 60, 65, 80}, {40, 90, 100, 150, 130}}; // matriz de cada costo que hay 
 
-        EsquinaNoroeste case1 = new EsquinaNoroeste(3, 4); // n es la cantidad de ofertas y m la cantidad de demandas 
+        EsquinaNoroeste case1 = new EsquinaNoroeste(3, 5); // n es la cantidad de ofertas y m la cantidad de demandas 
         case1.setD(D);
         case1.setO(O);
         case1.setC(MatrizCostos);
